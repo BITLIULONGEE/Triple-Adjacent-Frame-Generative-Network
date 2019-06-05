b14b98975a692c1912cda1075fe0a49b98c43852
BITLIULONGEE <51401349+BITLIULONGEE@users.noreply.github.com>
2019-06-05 20:44:17 +0800

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -8,14 +8,11 @@ import os
 
 def split_input(img):      # split the image into 3*blurrys + target 
     img = img/ 127.5 - 1.0
-    #h = img.shape[0] // 2
-    #w = img.shape[1] // 2
-    #blurry = [img[:h, :w, :], img[:h, w:, :], img[h:, :w, :]]
-    #sharp = [img[h:, w:, :]]
-    #return blurry + sharp
-    h = img.shape[0] // 3
-    blurry = [img[:h, :, :], img[h:2*h, :, :], img[2*h:, :, :]]
-    return blurry         
+    h = img.shape[0] // 2
+    w = img.shape[1] // 2
+    blurry = [img[:h, :w, :], img[:h, w:, :], img[h:, :w, :]]
+    sharp = [img[h:, w:, :]]
+    return blurry + sharp     
 
 def get_data(datadir, phase='train', crop_size=256, batch_size=1):
 
